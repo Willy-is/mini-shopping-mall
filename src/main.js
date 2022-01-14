e7619419943a21fcfd47047756416ab55d06e436
William Kim <57608628+Willy-is@users.noreply.github.com>
2022-01-14 19:20:45 +1100

    JS: Update the items object to li strings; Create event handler for buttons clicked

--- src/main.js
+++ src/main.js
@@ -7,12 +7,14 @@ function loadItems() {
     .then((json) => json.items);
 }
 
+// Update the list with the given items (object -> li strings)
 function displayItems(items) {
   // https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML
   const container = document.querySelector('.items');
   container.innerHTML = items.map((item) => createHTMLString(item)).join('');
 }
 
+// Create HTML list item for the given data item
 function createHTMLString(item) {
   return `
     <li class="item">
@@ -21,9 +23,29 @@ function createHTMLString(item) {
     </li>`;
 }
 
+// Filter items when onButtonClick listener occurs
+function onButtonClick(event, items) {
+  // https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes
+  const target = event.target;
+  const key = target.dataset.key;
+  const value = target.dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+  displayItems(items.filter((item) => item[key] === value));
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector('.logo');
+  const buttons = document.querySelector('.buttons');
+  logo.addEventListener('click', () => displayItems(items));
+  buttons.addEventListener('click', (event) => onButtonClick(event, items));
+}
+
 loadItems()
   .then((items) => {
     displayItems(items);
-    // setEventListeners();
+    setEventListeners(items);
   })
-  .catch((error) => new Error(''));
+  .catch(new Error('Items not found'));
